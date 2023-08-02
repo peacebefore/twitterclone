5d0560a4ee5d163e23cc54c48ec8fb4e413613c1
peacebefore <peacebefore@gmail.com>
2023-08-01 17:10:10 -0700

    created a drawer and settings page

--- twitterclone/lib/pages/home.dart
+++ twitterclone/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitterclone/pages/settings.dart';
 import 'package:twitterclone/providers/user_provider.dart';
 
 class Home extends ConsumerWidget {
@@ -8,31 +9,59 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-        actions: [
-          TextButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              ref.read(userProvider.notifier).logout();
-            },
-            child: const Text(
-              "Sign Out",
-              style: TextStyle(color: Colors.white),
+        leading: Builder(builder: (context) {
+          return GestureDetector(
+            onTap: () => Scaffold.of(context).openDrawer(),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(currentUser.user.profilePic),
+              ),
             ),
-          )
-        ],
+          );
+        }),
+        title: const Text("Home"),
       ),
       body: Column(
         children: [
-          Text(ref.watch(userProvider).user.email),
-          Text(ref.watch(userProvider).user.name),
-          CircleAvatar(
-            backgroundImage:
-                NetworkImage(ref.watch(userProvider).user.profilePic),
-          ),
+          Text(currentUser.user.email),
+          Text(currentUser.user.name),
         ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Image.network(currentUser.user.profilePic),
+            ListTile(
+              title: Text(
+                "Hello, ${currentUser.user.name}",
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const Settings(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Sign Out"),
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+                ref.read(userProvider.notifier).logout();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
